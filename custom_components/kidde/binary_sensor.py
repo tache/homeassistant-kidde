9bae00f2ccdf6f349f25388c80ce9f889837f47e
Nick Berardi <nberardi@gmail.com>
2024-07-26 15:03:53 -0400

    Add battery voltage and wifi connectivity sensors and moved battery_state to binary_sensor

--- custom_components/kidde/binary_sensor.py
+++ custom_components/kidde/binary_sensor.py
@@ -18,21 +18,39 @@ from .entity import KiddeEntity
 
 _BINARY_SENSOR_DESCRIPTIONS = (
     BinarySensorEntityDescription(
-        "smoke_alarm",
+        key="smoke_alarm",
         icon="mdi:smoke-detector-variant-alert",
         name="Smoke Alarm",
         device_class=BinarySensorDeviceClass.SMOKE,
     ),
     BinarySensorEntityDescription(
-        "smoke_hushed",
+        key="smoke_hushed",
         icon="mdi:smoke-detector-variant-off",
         name="Smoke Hushed",
     ),
     BinarySensorEntityDescription(
-        "co_alarm",
+        key="co_alarm",
         icon="mdi:molecule-co",
         name="CO Alarm",
         device_class=BinarySensorDeviceClass.CO,
+    ),
+)
+
+_INVERSE_BINARY_SENSOR_DESCRIPTIONS = (
+    BinarySensorEntityDescription(
+        key="offline",
+        icon="mdi:wifi-alert",
+        name="Online",
+        device_class=BinarySensorDeviceClass.CONNECTIVITY,
+    ),
+)
+
+_BATTERY_SENSOR_DESCRIPTIONS = (
+    BinarySensorEntityDescription(
+        key="battery_state",
+        icon="mdi:battery-alert",
+        name="Battery State",
+        device_class=BinarySensorDeviceClass.BATTERY
     ),
 )
 
@@ -48,6 +66,14 @@ async def async_setup_entry(
             sensors.append(
                 KiddeBinarySensorEntity(coordinator, device_id, entity_description)
             )
+        for entity_description in _INVERSE_BINARY_SENSOR_DESCRIPTIONS:
+            sensors.append(
+                KiddeInverseBinarySensorEntity(coordinator, device_id, entity_description)
+            )
+        for entity_description in _BATTERY_SENSOR_DESCRIPTIONS:
+            sensors.append(
+                KiddeBatteryStateSensorEntity(coordinator, device_id, entity_description)
+            )
     async_add_devices(sensors)
 
 
@@ -58,3 +84,19 @@ class KiddeBinarySensorEntity(KiddeEntity, BinarySensorEntity):
     def is_on(self) -> bool | None:
         """Return the value of the binary sensor."""
         return self.kidde_device.get(self.entity_description.key)
+
+class KiddeInverseBinarySensorEntity(KiddeEntity, BinarySensorEntity):
+    """Binary sensor for Kidde HomeSafe."""
+
+    @property
+    def is_on(self) -> bool | None:
+        """Return the value of the binary sensor."""
+        return self.kidde_device.get(self.entity_description.key) == False
+
+class KiddeBatteryStateSensorEntity(KiddeEntity, BinarySensorEntity):
+    """Binary sensor for Kidde HomeSafe."""
+
+    @property
+    def is_on(self) -> bool | None:
+        """Return the value of the binary sensor."""
+        return self.kidde_device.get(self.entity_description.key) != "ok"
